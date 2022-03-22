16dfa477c972d9d02087a9a18cc8030bed595744
Maria Varabyova <m.warabjowa@gmail.com>
2022-03-22 14:01:40 +0300

    Add task 2 (Hello+Name)

---
+++ src/homework/task02/HW2.java
@@ -0,0 +1,15 @@
+package homework.task02;
+
+import java.util.Scanner;
+
+public class HW2 {
+     public static void main (String args[]) {
+
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Введите ваше имя.");
+
+                  String name = sc.nextLine();
+         System.out.println("Привет, " + name.toString()+"!");
+
+     }
+}
