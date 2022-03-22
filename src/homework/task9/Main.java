bfb928cc13ba540f167afb71f73e201175881739
Maria Varabyova <m.warabjowa@gmail.com>
2022-03-22 13:45:10 +0300

    Add task 9

---
+++ src/homework/task9/Main.java
@@ -0,0 +1,16 @@
+package homework.task9;
+
+public class Main {
+
+    public static void main (String args[]) {
+        Vector One = new Vector(2 , 4);
+        Vector Two = new Vector (5, 8);
+
+
+
+        System.out.println ("=========");
+        System.out.println(One);
+    }
+
+
+}
