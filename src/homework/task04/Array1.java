08c2340c250fbbad5c79647269de19194d8719a3
Maria Varabyova <m.warabjowa@gmail.com>
2022-04-27 13:33:33 +0300

    Fix bug in task4.Array1.

--- src/homework/task04/Array1.java
+++ src/homework/task04/Array1.java
@@ -14,45 +14,76 @@ public class Array1 {
 
         Random rd = new Random();
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = rd.nextInt(101);
+            arr[i] = rd.nextInt(11);
         }
         System.out.print("Исходный массив: ");
         System.out.println(Arrays.toString(arr));
 
 
-        System.out.print("Введите число от 0 до 100: ");
+        System.out.print("Введите число от 0 до 10: ");
         Scanner sc = new Scanner(System.in);
         int elementToDelete = sc.nextInt();
 
-        if ((contains(arr, elementToDelete))==true){
-            System.out.println("Новый массив: "+Arrays.toString(createNewArr(arr, elementToDelete)));
-        }else{
+        if ((counts(arr, elementToDelete))==0){
             System.out.println("Число не входит в массив.");
+            return;
+        }else{
+            System.out.println("Число вхождений элемента в массив = "+counts(arr, elementToDelete));
+            System.out.println("Новый массив: "+Arrays.toString(createNewArr(arr, elementToDelete)));
+
         }
     }
 
-    static boolean contains(int[] arr, int key){
-        for (int i : arr) {
-            if (i == key) {
-                return true;
+    static int counts (int[] arr, int key){
+        int count=0;
+        for (int i: arr){
+            if (i==key) {
+                count++;
             }
         }
-        return false;
+        return count;
+    }
+
+    static int findSize (int arr[], int elementToDelete){
+        int size = 0;
+        for(int i=0; i<arr.length; i++){
+            if (elementToDelete==arr[i]){
+                continue;
+            }else{
+                size++;
+            }
+        }
+        return size;
     }
 
     public static int[] createNewArr(int arr[], int elementToDelete){
-        int[] newArr = null;
-        for (int i = 0; i < arr.length - 1; i++) {
+
+        int size = findSize(arr, elementToDelete);
+
+        int[] newArr = new int[size];
+        for (int i = 0, j = 0; j < newArr.length; i ++, j ++) {
             if (arr[i] == elementToDelete) {
-                newArr = new int[arr.length - 1];
-                for (int index = 0; index < i; index++) {
-                    newArr[index] = arr[index];
-                }
-                for (int j = i; j < arr.length - 1; j++) {
-                    newArr[j] = arr[j + 1];
-                }
+                newArr[i]=arr[j++];
+            } else {
+                newArr[ i ] = arr [ j ]; // баг, если вхождения элементов находятся последовательно.
+                // например 8, 7, 7, 9, 6 - пр удалении 7 выходит следующий ответ: 8,7,9
             }
         }
         return newArr ;
     }
 }
+
+
+//int[] newArr = null;
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            if (arr[i] == elementToDelete) {
+//                newArr = new int[arr.length - 1];
+//                for (int index = 0; index < i; index++) {
+//                    newArr[index] = arr[index];
+//                }
+//                for (int j = i; j < arr.length - 1; j++) {
+//                    newArr[j] = arr[j + 1];
+//                }
+//            }
+//        }
+//        return newArr ;
